a277f5e15320f3483625c9400a591e6d7b31a430
francis <willenerfrancis0@gmail.com>
2023-08-16 14:01:41 -0300

    Ajuste de propiedad

--- components/News.js
+++ components/News.js
@@ -20,7 +20,7 @@ const [news, setNews] = useState([])
           <h2 className='text-white text-center mt-12 md:text-3xl md:mt-16 mb-1 font-light text-2xl'>Novedades del Club</h2>
           <div className='flexContainer'>
               {news.map((n) => (
-                <NewBox key={n._id} oneNew={...n}/>
+                <NewBox key={n._id} oneNew={n}/>
               ))}
           </div>
         </div>
