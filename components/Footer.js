4f4720887cb05d8f08696fa8d1f82df251a0980f
francis <willenerfrancis0@gmail.com>
2023-12-08 16:44:01 -0300

    Modificacion footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,24 +8,30 @@ function Footer() {
           <div className="flex flex-col lg:flex-row lg:justify-center">
             <div className="flex flex-col lg:grid lg:grid-cols-2 items-start gap-8 ml-3 mt-5 text-white">
               <a href="#" className="text-lg font-ligh cursor-default">
-                <i className="bx bxs-map"></i><p className="text-gray-300 ml-2 inline text-sm">RN34 220, Rafaela, Santa Fe</p>
-              </a>
-              <a className="text-xl font-light" href="tel:+54 9 3492 42-2219">
-                <i className="bx bxs-phone"></i><p className="text-gray-300 ml-2 inline text-sm hover:text-ligthblue transition-all">+54 9 3492 42-2219</p>
+                <i className="bx bxs-map"></i>
+                <p className="text-gray-300 ml-2 inline text-sm">
+                  RN34 220, Rafaela, Santa Fe
+                </p>
               </a>
               <a
                 className="text-xl font-light"
                 href="https://www.instagram.com/aeroclubrafaela_/"
                 target="_BLANCK"
               >
-                <i className="bx bxl-instagram"></i><p className="text-gray-300 ml-2 inline text-sm hover:text-ligthblue transition-all">/aeroclubrafaela_</p>
+                <i className="bx bxl-instagram"></i>
+                <p className="text-gray-300 ml-2 inline text-sm hover:text-ligthblue transition-all">
+                  /aeroclubrafaela_
+                </p>
               </a>
               <a
-                className="text-xl font-light"
+                className="text-xl font-light lg:col-span-2 text-center"
                 href="https://www.facebook.com/search/top?q=aero%20club%20rafaela"
                 target="_BLANCK"
               >
-                <i className="bx bxl-facebook-square"></i><p className="text-gray-300 ml-2 inline text-sm hover:text-ligthblue transition-all">/AeroRafaela</p>
+                <i className="bx bxl-facebook-square"></i>
+                <p className="text-gray-300 ml-2 inline text-sm hover:text-ligthblue transition-all">
+                  /AeroRafaela
+                </p>
               </a>
             </div>
             <div className="lg:hidden h-px bg-gray-400 my-8"></div>
@@ -39,15 +45,28 @@ function Footer() {
           </div>
           <div className="mx-auto h-px w-full xl:w-10/12 bg-gray-400 my-8"></div>
           <div className="mb-4">
-            <p className="text-sm mx-auto w-4/5 text-center">Diseñador Web: Francis Willener</p>
+            <p className="text-sm mx-auto w-4/5 text-center">
+              Diseñador Web: Francis Willener
+            </p>
             <div className="flex justify-center items-center gap-3 mt-3">
-              <a href="mailto:willenerfrancis0@gmail.com" className="bg-gray-300 py-1 px-2 rounded-full text-moredark hover:text-red-400 transition-all">
+              <a
+                href="mailto:willenerfrancis0@gmail.com"
+                className="bg-gray-300 py-1 px-2 rounded-full text-moredark hover:text-red-400 transition-all"
+              >
                 <i className="bx bxl-gmail"></i>
               </a>
-              <a href="https://github.com/chesquinio" target="_BLANCK" className="bg-gray-300 py-1 px-2 rounded-full text-moredark hover:text-black transition-all">
+              <a
+                href="https://github.com/chesquinio"
+                target="_BLANCK"
+                className="bg-gray-300 py-1 px-2 rounded-full text-moredark hover:text-black transition-all"
+              >
                 <i className="bx bxl-github"></i>
               </a>
-              <a href="https://www.linkedin.com/in/francis-willener-38369320b/" target="_BLANCK" className="bg-gray-300 py-1 px-2 rounded-full text-moredark hover:text-blue-400 transition-all">
+              <a
+                href="https://www.linkedin.com/in/francis-willener-38369320b/"
+                target="_BLANCK"
+                className="bg-gray-300 py-1 px-2 rounded-full text-moredark hover:text-blue-400 transition-all"
+              >
                 <i className="bx bxl-linkedin"></i>
               </a>
             </div>
--- pages/aeronautica/reservas.js
+++ pages/aeronautica/reservas.js
@@ -1,9 +1,124 @@
-import React from 'react'
+import NoSsr from "@/components/NoSsr";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import Calendar from "react-calendar";
+import { format, isEqual } from "date-fns";
+import { getTimes } from "@/lib/getTime";
 
-function AeroClubForm() {
+function FlyReservationPage() {
+  const [date, setDate] = useState({});
+  const [currentForm, setCurrentForm] = useState(1);
+  const [isModal, setIsModal] = useState(false);
+  const [message, setMessage] = useState(null);
+
+  const times = getTimes({ justDate: date.justDate });
+
+  const goBack = () => {
+    if (currentForm > 1) {
+      setCurrentForm(currentForm - 1);
+      setDate({});
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>AeroClubForm</div>
-  )
+    <NoSsr>
+      <motion.div
+        className="bg-white rounded-lg sm:shadow-xl px-8 py-2 mt-10 sm:mt-24 sm:mb-10 w-full sm:w-3/5 xl:w-2/5 2xl:w-1/3 mx-auto text-sm sm:text-base"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <form onSubmit={handleSubmit}>
+          {currentForm > 1 && (
+            <button
+              type="button"
+              onClick={goBack}
+              className="backButton w-10 h-10 text-gray-700 text-4xl rounded"
+            >
+              <i className="bx bx-left-arrow-alt"></i>
+            </button>
+          )}
+
+          {currentForm === 1 && (
+            <motion.section
+              initial={{ x: "100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+            >
+              <h1 className="font-light text-center text-3xl mb-6">
+                Elije una fecha:
+              </h1>
+              <div className="w-full justify-center items-center">
+                <Calendar
+                  minDate={new Date()}
+                  view="month"
+                  onClickDay={(date) =>
+                    setDate((prev) => ({ ...prev, justDate: date }))
+                  }
+                />
+              </div>
+            </motion.section>
+          )}
+
+          {currentForm === 2 && (
+            <motion.section
+              initial={{ x: "100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+            >
+              <h3 className="text-3xl text-center font-light mb-6">
+                Horarios disponibles:
+              </h3>
+              <div className="grid grid-col-1 md:grid-cols-3 text-center gap-5">
+                {times?.map((time, i) => (
+                  <button
+                    key={`time-${i}`}
+                    type="button"
+                    onClick={() =>
+                      setDate((prev) => ({ ...prev, dateTime: time }))
+                    }
+                    className={`py-8 px-4 rounded text-xl ${
+                      isEqual(date.dateTime, time)
+                        ? "bg-blue-400 text-white"
+                        : "bg-whiteblue text-gray-800 hover:bg-blue-200 hover:text-white"
+                    } transition duration-300 ease-in-out`}
+                  >
+                    {format(time, "kk:mm")}
+                  </button>
+                ))}
+              </div>
+            </motion.section>
+          )}
+          {message && <p className="text-gray my-2">{message}</p>}
+          {currentForm > 1 ? (
+            <button
+              type="submit"
+              disabled={!date.dateTime}
+              className={`text-white w-full text-center text-xl py-3 rounded my-4 ${
+                !date.dateTime ? "bg-blue-200" : "bg-blue-400"
+              }`}
+            >
+              Enviar
+            </button>
+          ) : (
+            <button
+              onClick={() => setCurrentForm(currentForm + 1)}
+              disabled={!date.justDate}
+              className={`text-white w-full text-center text-xl py-3 rounded my-4 ${
+                !date.justDate ? "bg-blue-200" : "bg-blue-400"
+              }`}
+            >
+              Siguiente
+            </button>
+          )}
+        </form>
+      </motion.div>
+    </NoSsr>
+  );
 }
 
-export default AeroClubForm
+export default FlyReservationPage;
